1bc17f6b1af99a963e449137e44fd9dca283cf84
yutoi-git <yuto.i0912@gmail.com>
2022-05-30 21:19:06 +0900

    Add: ユーザー登録機能とログイン機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'top_page#top'
+  root to: 'top_pages#top'
+  get '/login', to: 'user_sessions#new'
+  post '/login', to: 'user_sessions#create'
+  delete '/logout', to: 'user_sessions#destroy'
+  resources :users, only: %i[new create]
 end
