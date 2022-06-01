56b06ac8d6cb16942f6a05841130bdf64a30078a
yutoi-git <yuto.i0912@gmail.com>
2022-06-01 17:42:28 +0900

    Add: ゲストログイン機能の追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -19,6 +19,19 @@ class UserSessionsController < ApplicationController
     redirect_to(login_url, notice: 'Logged out!')
   end
 
+  def guest_login
+    rondom_value = SecureRandom.alphanumeric(10) + Time.zone.now.to_i.to_s
+    @guest_user = User.create(
+      email: rondom_value + '@example.com',
+      password: 'password',
+      password_confirmation: 'password',
+      role: :guest
+    )
+    #@guest_user.update!
+    auto_login(@guest_user)
+    redirect_back_or_to root_path, success: 'ゲストとしてログインしました'
+  end
+
   private
 
   def user_params
