bb78063730a4bfa367b2fd7ca36f34ffcf5f9b26
yutoi-git <yuto.i0912@gmail.com>
2022-07-06 14:35:49 +0900

    Add: プロフィール編集機能を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_173429) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_06_052412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_21_173429) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0, null: false
+    t.string "myself_gender"
+    t.integer "myself_age"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
